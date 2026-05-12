12c673ece191488a045abc8d67c1ae2c72406a44
qerst2508 <qerst_2508@outlook.com>
2026-05-12 15:25:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello from Azure App Service!"
+    return "Double Hello from Azure App Service!"
 
 @app.route("/health")
 def health():
